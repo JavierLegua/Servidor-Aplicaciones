b1c62c84870fd4c520569ffafdc39522787b43b4
Javier Legua <javierlegua14@gmail.com>
2021-10-20 10:37:27 +0200

    Buenos cambios

--- index.php
+++ index.php
@@ -28,7 +28,40 @@
                     if ($archivo != "." && $archivo != ".."){
                         //Sii es un directorio se recorre recursivamente
                         if(is_dir($ruta_completa)){
-                            echo "<li> <a href='".$_SERVER['PHP_SELF']."?categoria=.$archivo'>".$archivo."</a></li>";
+                            echo "<li> <a href='".$_SERVER['PHP_SELF']."?categoria=$archivo'>".$archivo."</a></li>";
+                        }else{
+                            echo "<li>".$archivo."</li>";
+                        }
+                    }
+                }
+                //Cerramos el gestor de directorios
+                closedir($gestor);
+                echo "</ul>";
+            }else{
+                echo "La ruta introducida no es valida";
+            }
+         }
+
+         //Función para comprobar si hay archivos dentro de una carpeta
+         function mostrarCarpeta($ruta, $archivo){
+            //Concatenas la ruta de antes con la ruta seleccionada
+            $ruta .= "/".$archivo;
+
+             //Se comprueba que la ruta sea un directorio
+             if(is_dir($ruta)){
+                //Se abre un gestor de directorios para la ruta indicada
+                $gestor = opendir($ruta);
+                echo "<ul style = 'list-style: none;'>";
+
+                //Recorre todos los elementos del directorio
+                while (($archivo = readdir($gestor)) !== false){
+                    $ruta_completa = $ruta."/".$archivo;
+
+                    //Se muestran todos los archivos y carpetas excepto "." y ".."
+                    if ($archivo != "." && $archivo != ".."){
+                        //Sii es un directorio se recorre recursivamente
+                        if(is_dir($ruta_completa)){
+                            echo "<li> <a href='".$_SERVER['PHP_SELF']."?categoria=$archivo'>".$archivo."</a></li>";
                             mostrarDirectorios($ruta_completa);
                         }else{
                             echo "<li>".$archivo."</li>";
@@ -42,7 +75,16 @@
                 echo "La ruta introducida no es valida";
             }
          }
-         mostrarDirectorios("aplicaciones");
+
+         //Mira si la categoria esta definida y si lo esta muestra lo que hay dentro
+         if (isset($_GET['categoria'])) {
+            $archivo = $_GET['categoria'];
+            mostrarCarpeta("aplicaciones", $archivo);
+        }else{
+            mostrarDirectorios("aplicaciones");
+        }
     ?>
+
+    
 </body>
 </html>
